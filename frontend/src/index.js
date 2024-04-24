65e3fe6ce91c26ae42469cef3b3435354425e0aa
Rakesh Das <157940757+RakeshDas18@users.noreply.github.com>
2024-04-24 10:38:42 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,7 +10,7 @@ import { persistStore } from 'redux-persist';
 
 let persistor = persistStore(store);
 
-export const BASE_URL="https://chat-app-rd.onrender.com"
+export const BASE_URL="https://chat-app-fr2p.onrender.com"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
