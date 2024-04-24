c2ebaa931f63b8b083f2765b60b51e302923bc38
Rakesh Das <157940757+RakeshDas18@users.noreply.github.com>
2024-04-24 10:34:50 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json()); 
 app.use(cookieParser());
 const corsOption={
-    origin:'http://localhost:3000',
+    origin:'https://chat-app-rd.onrender.com',
     credentials:true
 };
 app.use(cors(corsOption)); 
