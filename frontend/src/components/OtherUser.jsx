398c05996378bd30ae31c0ab1783c2eb84d3248b
Rakesh Das <157940757+RakeshDas18@users.noreply.github.com>
2024-04-28 22:12:03 +0530

    Update OtherUser.jsx

--- frontend/src/components/OtherUser.jsx
+++ frontend/src/components/OtherUser.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useDispatch,useSelector } from "react-redux";
-import { setSelectedUser } from '../redux/userSlice.js';
+import { setSelectedUser } from '../redux/userSlice';
 
 const OtherUser = ({ user }) => {
     const dispatch = useDispatch();
