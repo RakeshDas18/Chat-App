147e8484a08d46182f8f8e04ba9a40b18f6455df
Rakesh Das <157940757+RakeshDas18@users.noreply.github.com>
2024-04-30 19:23:04 +0530

    Update useGetOtherUsers.jsx

--- frontend/src/hooks/useGetOtherUsers.jsx
+++ frontend/src/hooks/useGetOtherUsers.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import axios from "axios";
 import { useDispatch } from "react-redux";
-import { setOtherUsers } from '../redux/userSlice';
+import { setOtherUsers } from '../redux/userSlice.js';
 import { BASE_URL } from '..';
 
 const useGetOtherUsers = () => {
