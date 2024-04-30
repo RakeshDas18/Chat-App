1b251843cba20d0b2473494e3c571983e3989313
Rakesh Das <157940757+RakeshDas18@users.noreply.github.com>
2024-04-30 20:29:50 +0530

    Update HomePage.jsx

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -4,7 +4,7 @@ import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import MenuBar from './MenuBar';
 import OtherUsers from "./OtherUsers"
-import OtherUsers from "./OtherUser"
+import OtherUser from "./OtherUser"
 
 const HomePage = () => {
   const { authUser } = useSelector(store => store.user);
