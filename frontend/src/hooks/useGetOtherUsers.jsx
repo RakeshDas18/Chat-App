a9670ff1301c11ba08984b4f2081f7345159288d
Rakesh Das <157940757+RakeshDas18@users.noreply.github.com>
2024-05-01 10:33:16 +0530

    Update useGetOtherUsers.jsx

--- frontend/src/hooks/useGetOtherUsers.jsx
+++ frontend/src/hooks/useGetOtherUsers.jsx
@@ -11,7 +11,7 @@ const useGetOtherUsers = () => {
         const fetchOtherUsers = async () => {
             try {
                 axios.defaults.withCredentials = true;
-                const res = await axios.get(`${BASE_URL}/api/v1/user`);
+                const res = await axios.get(`${BASE_URL}/api/v1/user/`);
                 // store
                 console.log("other users -> ",res);
                 dispatch(setOtherUsers(res.data));
