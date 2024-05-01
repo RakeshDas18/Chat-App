0396d350ec2eabf72173a75b923bcdd7438b8ad8
Rakesh Das <157940757+RakeshDas18@users.noreply.github.com>
2024-05-01 10:52:50 +0530

    Update OtherUser.jsx

--- frontend/src/components/OtherUser.jsx
+++ frontend/src/components/OtherUser.jsx
@@ -24,7 +24,7 @@ const OtherUser = ({ user }) => {
 
         <>
 
-            <div onClick={() => selectedUserHandler(user)} className={` ${selectedUser?._id == user?._id ? 'bg-zinc-200 text-black' : 'text-white'} flex gap-2 hover:text-black items-center hover:bg-zinc-200 rounded p-2 cursor-pointer`}>
+            <div onClick={() => selectedUserHandler(user)} className={` ${selectedUser?._id == user?.id ? 'bg-zinc-200 text-black' : 'text-white'} flex gap-2 hover:text-black items-center hover:bg-zinc-200 rounded p-2 cursor-pointer`}>
 
                 <div className={`avatar ${isOnline ? 'online' : '' }`}>
 
