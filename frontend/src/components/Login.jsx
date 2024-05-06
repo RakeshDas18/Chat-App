16749dd6dd6d088fd4b0c9c5c36afdd23b96719a
Rakesh Das <157940757+RakeshDas18@users.noreply.github.com>
2024-05-06 19:51:14 +0530

    Update Login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -18,28 +18,23 @@ const Login = () => {
     e.preventDefault();
     try {
       const res = await axios.post(`${BASE_URL}/api/v1/user/login`, user, {
-        headers: {
+        headers:{
           'Content-Type': 'application/json'
         },
-        withCredentials: true
+        withCredentials:true
       });
-      navigate("/");
-      console.log(res);
-      dispatch(setAuthUser(res.data));
+      navigate("/")
+      dispatch(setAuthUser(res.data))
     } catch (error) {
-      toast.error(error.response.data.message);
       console.log(error);
     }
-    setUser({
-      username: "",
-      password: ""
-    })
   }
   return (
     <div className="min-w-96 mx-auto">
       <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10 border border-gray-100'>
         <h1 className='text-3xl font-bold text-center'>Login</h1>
         <form onSubmit={onSubmitHandler} action="">
+
           <div>
             <label className='label p-2'>
               <span className='text-base label-text'>Username</span>
