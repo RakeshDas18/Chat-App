d73b1aa2574f199f96c4615a1fcbeeb7a6dcb78e
Rakesh Das <157940757+RakeshDas18@users.noreply.github.com>
2024-05-11 20:43:55 +0530

    Update OtherUser.jsx

--- frontend/src/components/OtherUser.jsx
+++ frontend/src/components/OtherUser.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useDispatch, useSelector } from "react-redux";
 import { setSelectedUser } from '../redux/userSlice';
 
-const OtherUser = ( user ) => {
+const OtherUser = ( {user} ) => {
     const dispatch = useDispatch();
     const { selectedUser, onlineUsers } = useSelector(store => store.user);
     const isOnline = onlineUsers?.includes(user?._id); // Guard against user being undefined
