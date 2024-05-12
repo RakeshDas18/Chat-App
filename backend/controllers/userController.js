f2e46ae739c818025bc74bc10d9cbf2919cd707d
Rakesh Das <157940757+RakeshDas18@users.noreply.github.com>
2024-05-12 12:18:52 +0530

    Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -14,7 +14,7 @@ export const register = async (req, res) => {
 
         const user = await User.findOne({ username });
         if (user) {
-            return res.status(400).json({ message: "Username already exit try different" });
+            return res.status(400).json({ message: "Username already exist. Try different username!!!" });
         }
         const hashedPassword = await bcrypt.hash(password, 10);
 
