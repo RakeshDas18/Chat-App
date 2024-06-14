8f874ceeca289d2710311cbed0d390aacefea900
Rakesh Das <157940757+RakeshDas18@users.noreply.github.com>
2024-06-14 19:46:21 +0530

    Update userRoute.js

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -7,6 +7,6 @@ const router = express.Router();
 router.route("/register").post(register);
 router.route("/login").post(login);
 router.route("/logout").get(logout);
-router.route("/").get(isAuthenticated,getOtherUsers);
+router.route("/").get(isAuthenticated, getOtherUsers);
 
 export default router;
