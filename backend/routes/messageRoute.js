a04e2138154a9ebca296e4ce3a99109180f1e57f
Rakesh Das <157940757+RakeshDas18@users.noreply.github.com>
2024-06-14 19:46:37 +0530

    Update messageRoute.js

--- backend/routes/messageRoute.js
+++ backend/routes/messageRoute.js
@@ -4,7 +4,7 @@ import isAuthenticated from "../middleware/isAuthenticated.js";
 
 const router = express.Router();
 
-router.route("/send/:id").post(isAuthenticated,sendMessage);
+router.route("/send/:id").post(isAuthenticated, sendMessage);
 router.route("/:id").get(isAuthenticated, getMessage);
 
 export default router;
