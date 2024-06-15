0abf8eda42a39d8acecfce1a71856944d9e6ece0
Rakesh Das <157940757+RakeshDas18@users.noreply.github.com>
2024-06-15 17:34:55 +0530

    Update OtherUser.jsx

--- frontend/src/components/OtherUser.jsx
+++ frontend/src/components/OtherUser.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import React from 'react';
 import { useDispatch, useSelector } from "react-redux";
 import { setSelectedUser } from '../redux/userSlice';
 
-const OtherUser = ( { user } ) => {
+const OtherUser = ({ user }) => {
     const dispatch = useDispatch();
     const { selectedUser, onlineUsers } = useSelector(store => store.user);
     const isOnline = onlineUsers?.includes(user?._id); 
@@ -11,10 +11,12 @@ const OtherUser = ( { user } ) => {
     }
 
     if (!user) {
-        <p>YOU ARE NOT THE USER!!!</p>
+        return <p>YOU ARE NOT THE USER!!!</p>;
+    }
+
     return (
         <>
-            <div onClick={() => selectedUserHandler(user)} className={`${selectedUser?._id == user?._id ? 'bg-zinc-200 text-black' : 'text-white'} flex gap-2 hover:text-black items-center hover:bg-zinc-200 rounded p-2 cursor-pointer`}>
+            <div onClick={() => selectedUserHandler(user)} className={`${selectedUser?._id === user?._id ? 'bg-zinc-200 text-black' : 'text-white'} flex gap-2 hover:text-black items-center hover:bg-zinc-200 rounded p-2 cursor-pointer`}>
                 <div className={`avatar ${isOnline ? 'online' : ''}`}>
                     <div className='w-12 rounded-full'>
                         <img src={user?.profilePhoto} alt="user-profile" />
@@ -28,7 +30,7 @@ const OtherUser = ( { user } ) => {
             </div>
             <div className='divider my-0 py-0 h-1'></div>
         </>
-    )
+    );
 }
 
 export default OtherUser;
