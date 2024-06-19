688d68d35d63843aff2f7bf7a3e457fad957207d
Rakesh Das <157940757+RakeshDas18@users.noreply.github.com>
2024-06-19 19:51:41 +0530

    Update Login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import toast from "react-hot-toast"
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import toast from "react-hot-toast";
 import axios from "axios";
 import { useDispatch } from "react-redux";
 import { setAuthUser } from '../redux/userSlice';
