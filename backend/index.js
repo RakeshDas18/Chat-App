2a8c3b955f4aed08b72617293cbb97aa0ad29107
Rakesh Das <157940757+RakeshDas18@users.noreply.github.com>
2024-06-24 19:32:35 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,4 @@
-// const express = require('express')// method-1
-import express from "express"; // method-2
+import express from "express";
 import dotenv from "dotenv"; 
 import connectDB from "./config/database.js";
 import userRoute from "./routes/userRoute.js";
