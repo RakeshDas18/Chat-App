86fe366c3939cf67d96b5ab15f54c997378cddee
Rakesh Das <157940757+RakeshDas18@users.noreply.github.com>
2024-07-01 19:26:57 +0530

    Update OtherUsers.jsx

--- frontend/src/components/OtherUsers.jsx
+++ frontend/src/components/OtherUsers.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import OtherUser from './OtherUser';
 import useGetOtherUsers from '../hooks/useGetOtherUsers';
 import {useSelector} from "react-redux";
