eaf27db0cc38be8c106a8c44cefd0555757bebca
Rakesh Das <157940757+RakeshDas18@users.noreply.github.com>
2024-07-09 10:56:48 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import { Toaster } from "react-hot-toast";
 import { Provider } from "react-redux";
 import store from './redux/store';
-import { PersistGate } from 'redux-persist/integration/react'
+import { PersistGate } from 'redux-persist/integration/react';
 import { persistStore } from 'redux-persist';
 
 let persistor = persistStore(store);
