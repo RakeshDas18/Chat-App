ab73119a165f441067a67a1c598d40c3096dce44
Rakesh Das <157940757+RakeshDas18@users.noreply.github.com>
2024-08-04 19:43:45 +0530

    Update isAuthenticated.js

--- backend/middleware/isAuthenticated.js
+++ backend/middleware/isAuthenticated.js
@@ -3,7 +3,7 @@ const isAuthenticated = async(req,res,next) => {
   try {
     const token = req.cookies.token;
     if(!token){
-        return res.status(401).json({message:"User not authenticated."})
+        return res.status(401).json({message:"User not authenticated."});
     };
     const decode = await jwt.verify(token,process.env.JWT_SECERET_KEY);
     if(!decode){
