e83f62e6c9b8e0b400eaf071aacb82f618661159
Rakesh Das <157940757+RakeshDas18@users.noreply.github.com>
2024-08-17 19:24:27 +0530

    Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -18,7 +18,7 @@ export const register = async (req, res) => {
         }
         const hashedPassword = await bcrypt.hash(password, 10);
 
-        // profilePhoto
+        // Profile Photo
         const maleProfilePhoto = `https://avatar.iran.liara.run/public/boy?username=${username}`;
         const femaleProfilePhoto = `https://avatar.iran.liara.run/public/girl?username=${username}`;
 
