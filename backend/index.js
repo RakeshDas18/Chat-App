ca6d09564915dfe53b57886be96123061252ce6a
Rakesh Das <157940757+RakeshDas18@users.noreply.github.com>
2024-09-29 21:56:48 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.urlencoded({extended:true}));
 app.use(express.json()); 
 app.use(cookieParser());
-const corsOption={
+const corsOption = {
     origin:'https://localhost:8000',
     credentials:true
 };
