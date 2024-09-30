125fad6b26b7b67b82026d145d66674311341b0b
Rakesh Das <157940757+RakeshDas18@users.noreply.github.com>
2024-09-30 22:18:57 +0530

    Fixed some bugs

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     await mongoose.connect(process.env.MONGO_URI).then(() => {
-        console.log("Database connected");
+        console.log("Database connected!");
     }).catch( (error) => {
         console.log(error);
     })
