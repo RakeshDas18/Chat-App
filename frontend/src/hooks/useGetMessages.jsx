4837583d8ac691107007b35eaba4d5a9fa1e4066
Rakesh Das <157940757+RakeshDas18@users.noreply.github.com>
2024-10-12 12:22:42 +0530

    Fixed some bugs

--- frontend/src/hooks/useGetMessages.jsx
+++ frontend/src/hooks/useGetMessages.jsx
@@ -1,18 +1,18 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from 'react';
 import axios from "axios";
 import {useSelector,useDispatch} from "react-redux";
 import { setMessages } from '../redux/messageSlice';
 import { BASE_URL } from '..';
 
 const useGetMessages = () => {
-    const {selectedUser} = useSelector(store=>store.user);
+    const {selectedUser} = useSelector(store => store.user);
     const dispatch = useDispatch();
     useEffect(() => {
         const fetchMessages = async () => {
             try {
                 axios.defaults.withCredentials = true;
                 const res = await axios.get(`${BASE_URL}/api/v1/message/${selectedUser?._id}`);
-                dispatch(setMessages(res.data))
+                dispatch(setMessages(res.data));
             } catch (error) {
                 console.log(error);
             }
