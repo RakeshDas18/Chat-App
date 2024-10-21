9fb53c4d74af8b30651654440f04eef5a4ff6337
Rakesh Das <157940757+RakeshDas18@users.noreply.github.com>
2024-10-21 20:43:10 +0530

    Fixed some bugs

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
     await mongoose.connect(process.env.MONGO_URI).then(() => {
         console.log("Database connected!");
     }).catch( (error) => {
-        console.log(error);
+        console.log(error)
     })
 };
-export default connectDB;
+export default connectDB
