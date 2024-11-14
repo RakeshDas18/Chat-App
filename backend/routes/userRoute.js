e6f6808db592888c63b915bf5dedbb59acc0cb5e
Rakesh Das <157940757+RakeshDas18@users.noreply.github.com>
2024-11-14 20:04:36 +0530

    Fixed some bugs

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -2,11 +2,11 @@ import express from "express";
 import { getOtherUsers, login, logout, register } from "../controllers/userController.js";
 import isAuthenticated from "../middleware/isAuthenticated.js";
 
-const router = express.Router();
+const router = express.Router()
 
 router.route("/register").post(register);
 router.route("/login").post(login);
-router.route("/logout").get(logout);
-router.route("/").get(isAuthenticated, getOtherUsers);
+router.route("/logout").get(logout)
+router.route("/").get(isAuthenticated, getOtherUsers)
 
 export default router;
